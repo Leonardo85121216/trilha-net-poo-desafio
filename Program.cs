6d7cf202ff0c0beb5c67db319a5aa464752b0af7
Leonardo Campana <leonardobcampana@gmail.com>
2024-04-15 21:23:27 -0300

    Desafio POO Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("36-632783446", "NK-CKL22", "CKJ3KSJ24NS89", 2);
+Iphone iphone = new Iphone("42-836222785", "IP-JK24N", "AKD3KSJ5BNS89", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Clash Royale");
+
+Console.WriteLine("\n\nMudando para o Iphone...\n\n");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Subway Surfers");
